3a802468df1fd2b80b8005b6f7bff1e00b55ad7a
akio0911 <akio0911@gmail.com>
2021-05-07 13:07:59 +0900

    feedback

--- Kadai5/ViewController.swift
+++ Kadai5/ViewController.swift
@@ -15,36 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet private weak var resultLabel: UILabel!
     
     @IBAction private func resultBtn(_ sender: Any) {
-        
-        //アラートを作る
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
-        alert.title = "課題5"
-        alert.addAction(
-            UIAlertAction(title: "OK", style: .default)
-        )
-        
-        //アラートメッセージを条件分岐させる
-        if textField1.text! == ""{
-            alert.message = "割られる数を入力して下さい"
-            self.present(alert, animated: true)
+        guard let num1 = Double(textField1.text!) else {
+            presentAlert(message: "割られる数を入力して下さい")
             return
-        } else if textField2.text! == ""{
-            alert.message = "割る数を入力して下さい"
-            self.present(alert, animated: true)
+        }
+
+        guard let num2 = Double(textField2.text!) else {
+            presentAlert(message: "割る数を入力して下さい")
             return
-        } else if textField2.text == "0"{
-            alert.message = "割る数に0を入力しないで下さい"
-            self.present(alert, animated: true)
+        }
+
+        guard num2 != 0 else {
+            presentAlert(message: "割る数に0を入力しないで下さい")
             return
         }
         
         //割り算する
-        let num1 = Double(textField1.text!) ?? 0
-        let num2 = Double(textField2.text!) ?? 0
-        resultLabel.text = String(num1/num2)
+        resultLabel.text = String(num1 / num2)
         
         //キーボード戻す
         view.endEditing(true)
     }
-}
 
+    private func presentAlert(message: String) {
+        //アラートを作る
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.title = "課題5"
+        alert.addAction(
+            UIAlertAction(title: "OK", style: .default)
+        )
+        present(alert, animated: true)
+    }
+}
